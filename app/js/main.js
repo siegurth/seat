c87bc2bd2a5c32bc7d0e5948b37f8c2547ba8797
siegurth <30534477+siegurth@users.noreply.github.com>
2018-02-20 13:19:17 +0200

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-	
+	$('.sandwich-menu').click(function(){
+		$(this).toggleClass('active');
+		$('nav').toggleClass('active');
+
+		return false;
+	});
+
+	$(window).on('resize', function(){
+		var w = $(window).width();
+
+		if (w >= 769) {
+			$('.sandwich-menu, nav').removeClass('active');
+		}
+	});
 });
